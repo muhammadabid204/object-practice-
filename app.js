5c2baa73847421c223f1852be8d44563159dac63
Muhammad Abid204 <decabidrafiq2008@gmail.com>
2025-03-22 00:50:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const student = {
+    fullname : "muhammad",
+    marks: "94.5",
+
+
+printmarks: function () {
+    console.log("marks =",this. marks)
+},
+};
+
+
+const  employee = {
+    calctax() {
+        console.log('tax rate is 10%');
+    }
+}
+
+const abid = {
+    salary: 50000
+}
